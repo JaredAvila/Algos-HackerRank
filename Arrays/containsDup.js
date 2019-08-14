50add03fe31bbd166a84b2b576d679797f8c68e0
Jared Avila <jared.c.avila@gmail.com>
2019-08-14 12:38:57 -0700

    contains duplicate with Map and Set

--- Arrays/containsDup.js
+++ Arrays/containsDup.js
@@ -10,6 +10,21 @@ const conatinsDuplicate = nums => {
   return false;
 };
 
-const arr = [1, 2, 3, 4, 1, 5, 7, 8];
+const conatinsDuplicateSet = nums => {
+  if (nums.length < 2) {
+    return false;
+  }
+  const set = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true;
+    } else {
+      set.add(nums[i]);
+    }
+  }
+  return false;
+};
 
-console.log(conatinsDuplicate(arr));
+const arr = [1, 2, 3, 4, 5, 7, 8];
+
+console.log(conatinsDuplicateSet(arr));
