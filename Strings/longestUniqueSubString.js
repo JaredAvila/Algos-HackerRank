8f6be3fb66132007552974f95bf9da920ff8d1af
Jared Avila <jared.c.avila@gmail.com>
2019-10-08 12:26:06 -0700

    finsihed longestUniqueSubString

--- Strings/longestUniqueSubString.js
+++ Strings/longestUniqueSubString.js
@@ -1,30 +1,23 @@
-// const mySet = new Set();
-
-// mySet.add("n");
-// mySet.add("o");
-// console.log(mySet);
-// mySet.clear();
-// console.log(mySet);
-
 const solution = s => {
-  let mySet = new Set();
+  let cache = {};
   let max = 1;
   let total = 0;
   for (let i = 0; i < s.length; i++) {
-    if (mySet.has(s[i])) {
+    if (s[i] in cache) {
+      i = cache[s[i]];
+      cache = {};
       if (total > max) {
         max = total;
       }
-      mySet.clear();
-      mySet.add(s[i]);
-      total = 1;
+      total = 0;
     } else {
-      mySet.add(s[i]);
+      cache[s[i]] = i;
       total++;
     }
   }
+  if (total > max) return total;
   return max;
 };
 
-const str = "nndfddf";
+const str = "asnrndfddf";
 console.log(solution(str));
